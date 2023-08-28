fef90166140613792e515cc6f3e186726c9d5962
tetat <nishat00101@gmail.com>
2023-08-28 13:16:29 +0600

    vecel json

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 
 // middleware
-app.use(cors({}));
+app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cookieParser());
@@ -37,3 +37,23 @@ mongoose
 app.get("*", checkUser);
 app.get("/", (req, res) => res.render("home"));
 app.use(router);
+
+/*
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxDP9ZSDza8AmK2cjwNYvavPNOxHU9e10",
+  authDomain: "ema-john-simple-95ec8.firebaseapp.com",
+  projectId: "ema-john-simple-95ec8",
+  storageBucket: "ema-john-simple-95ec8.appspot.com",
+  messagingSenderId: "718871516386",
+  appId: "1:718871516386:web:4564dbddbcaf2b7ced3abb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+ */
